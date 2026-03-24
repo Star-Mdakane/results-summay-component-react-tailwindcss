eecc4ec575567141fe7ec50b34488ddb743b8227
Star <mmdakane@rocketmail.com>
2026-03-24 21:08:47 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-400 font-bold underline">
+        Welcome to React!
+      </h1>
+    </>
+  )
+}
+
+export default App
